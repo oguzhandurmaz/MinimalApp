756c449fb5237782ddb30db1e1627d40aa8db82f
oguzhan.durmaz <oguzhan.durmaz@tekhnelogos.com>
2020-08-05 17:58:56 +0300

    #11 Added SearchBar and Search Functionality to Fav. Books UI

--- Android/MinimaApp/app/src/main/java/com/example/minimaapp/ui/FavoriteBooksFragment.kt
+++ Android/MinimaApp/app/src/main/java/com/example/minimaapp/ui/FavoriteBooksFragment.kt
@@ -1,18 +1,23 @@
 package com.example.minimaapp.ui
 
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.example.minimaapp.viewmodel.FavoriteBooksViewModel
 import com.example.minimaapp.IRecyclerOnClickListener
+import com.example.minimaapp.R
 import com.example.minimaapp.adapter.RecyclerViewFavoriteBooksAdapter
 import com.example.minimaapp.databinding.FragmentFavoriteBooksBinding
+import com.example.minimaapp.viewmodel.FavoriteBooksViewModel
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
@@ -45,6 +50,8 @@ class FavoriteBooksFragment : Fragment(), IRecyclerOnClickListener {
     private lateinit var viewModelFactory: FavoriteBooksViewModel.FavoriteBooksViewModelFactory
 
 
+    private lateinit var adapter: RecyclerViewFavoriteBooksAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -56,19 +63,77 @@ class FavoriteBooksFragment : Fragment(), IRecyclerOnClickListener {
             FavoriteBooksViewModel.FavoriteBooksViewModelFactory(
                 requireActivity().application
             )
-        viewModel = ViewModelProvider(this,viewModelFactory).get(FavoriteBooksViewModel::class.java)
+        viewModel =
+            ViewModelProvider(this, viewModelFactory).get(FavoriteBooksViewModel::class.java)
 
-        val adapter = RecyclerViewFavoriteBooksAdapter(this)
+        setHasOptionsMenu(true)
+
+        adapter = RecyclerViewFavoriteBooksAdapter(this)
         binding.recyclerFavBooks.adapter = adapter
 
+
         viewModel.favBooks.observe(viewLifecycleOwner, Observer {
-            it?.let {   //adapter.submitList(it.toMutableList())
+            it?.let {
+                //adapter.submitList(it.toMutableList())
                 adapter.setData(it)
             }
         })
 
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        val inputManager =
+            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+
+        inflater.inflate(R.menu.fav_books_menu, menu)
+
+        val searchMenu = menu.findItem(R.id.search_book)
+        val searchView = searchMenu.actionView as SearchView
+
+        searchMenu.setOnActionExpandListener(object : MenuItem.OnActionExpandListener{
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                viewModel.favBooks.value?.let {
+                    adapter.setData(it)
+                }
+                return true
+            }
+
+        })
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                //Filter ViewModel for query
+                val filteredList = viewModel.favBooks.value?.filter { bookTable ->
+                    bookTable.title.toLowerCase(Locale.getDefault()).contains(query.toString())
+                }
+
+                //Set Filter Result to Adapter
+                if (filteredList.isNullOrEmpty()) {
+                    Toast.makeText(requireContext(),"Eşleşen Kitap Bulunamadı",Toast.LENGTH_SHORT).show()
+                } else {
+                    adapter.setData(filteredList)
+                }
+
+                inputManager.hideSoftInputFromWindow(view?.windowToken, 0)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+        })
+
     }
 
 
